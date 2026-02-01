279159f2dada148398cad6ed4710391dbf02c15b
JaeKwan Ahn <52008@naver.com>
2026-02-01 13:20:11 +0900

    feat: next.config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // 프론트엔드에서 /api/... 로 요청하면
+        destination: "https://exchange-example.switchflow.biz/:path*", // 백엔드 서버로 연결해줍니다.
+      },
+    ];
+  },
 };
 
 export default nextConfig;
